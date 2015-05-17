37e71225a19410a0dd655972038e1d9e8c535d1a
Carlos Andonaegui <charlires@gmail.com>
2015-05-17 13:10:45 -0500

    List View Styles

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -56,21 +56,28 @@ class Main extends React.Component {
 
     renderMotel(motel) {
         return (
-            //<Image style={Styles.star} source={require('image!list_ic_price')}/>
             <View style={Styles.container}>
                 <View style={Styles.topContainer}>
                     <Text style={Styles.title}>Kuboz</Text>
+                    <View style={Styles.favContainer}>
+                        <Image style={Styles.favImg} source={require('image!list_fav')}/>
+                    </View>
                 </View>
                 <View style={Styles.bottomContainer}>
                     <View style={Styles.starsContainer}>
-                        <Image style={Styles.star} source={require('image!list_ic_star_active')}/>
-                        <Image style={Styles.star} source={require('image!list_ic_star_active')}/>
-                        <Image style={Styles.star} source={require('image!list_ic_star_active')}/>
-                        <Image style={Styles.star} source={require('image!list_ic_star_active')}/>
-                        <Image style={Styles.star} source={require('image!list_ic_star_active')}/>
+                        <Image style={Styles.starImg} source={require('image!list_ic_star_active')}/>
+                        <Image style={Styles.starImg} source={require('image!list_ic_star_active')}/>
+                        <Image style={Styles.starImg} source={require('image!list_ic_star_active')}/>
+                        <Image style={Styles.starImg} source={require('image!list_ic_star_active')}/>
+                        <Image style={Styles.starImg} source={require('image!list_ic_star_active')}/>
                     </View>
                     <View style={Styles.pricesContainer}>
-                        <Image style={Styles.prices} source={require('image!list_ic_price')}></Image>
+                        <Image style={Styles.pricesImg} source={require('image!list_ic_price')}/>
+                        <Text style={Styles.pricesText}>300 a 400</Text>
+                    </View>
+                    <View style={Styles.distanceContainer}>
+                        <Image style={Styles.distanceImg} source={require('image!list_ic_distance')}/>
+                        <Text style={Styles.pricesText}>2km</Text>
                     </View>
                 </View>
             </View>
