4f6b611221abbc9f9541b933012f208e1a69d0d9
Charlie <carlos.lopez@oncircle.com>
2015-05-17 17:22:43 -0500

    Adding global variables for styles and deleting old references to styles

--- App/globalvariables.js
+++ App/globalvariables.js
@@ -1,3 +1,10 @@
 /**
  * Created by Carlyeah on 5/17/15.
  */
+module.exports = {
+    rowBackground: '#22B8D8',
+    headerBackground: '#19CAB6',
+    orange: '#ff6204',
+    background: '#f5f5f5',
+    textColor: '#666',
+}
